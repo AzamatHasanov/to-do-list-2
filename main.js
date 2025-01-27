e7dccfb237c9b90862b386ce60048c2ed539d0ab
Azamat Qasimov <azamatqsimov@gmail.com>
2025-01-27 13:16:45 -0800

    todo v.1

---
+++ main.js
@@ -0,0 +1,21 @@
+let todo = document.getElementById("todo-input");
+let add = document.getElementById("add-btn");
+let list = document.getElementById("todo-list");
+
+function addToDo() {
+  const todoValues = todo.value.trim();
+  if (todoValues === "") return alert("inputu doldurun");
+
+  const li = document.createElement("li");
+  li.innerHTML = `<div class="mainflx">
+  <li>${todoValues}</li>
+    <span class="delete">✖</span></div>`;
+
+  li.addEventListener("click", function () {
+    this.classList.toggle("completed");
+  });
+
+  list.appendChild(li);
+  todo.value = "";
+}
+add.addEventListener("click", addToDo);
